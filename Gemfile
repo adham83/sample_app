0d9d0e965783c14d78644f6a0c425c7fe150bbbb
Adham El-Mohandes <adham83@gmail.com>
2014-03-04 09:11:39 +0200

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,26 @@
+source 'https://rubygems.org'
+
+gem 'rails', '4.0.2'
+
+group :development, :test do
+gem 'sqlite3'
+gem 'rspec-rails'
+end
+
+# Gems used only for assets and not required
+# in production environments by default.
+group :assets do
+gem 'sass-rails', '~>4.0.0'
+gem 'coffee-rails', '~>4.0.0'
+gem 'uglifier', '1.2.3'
+end
+
+gem 'jquery-rails'
+
+group :test do
+gem 'capybara'
+end
+
+group :production do
+gem 'pg'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SampleApp::Application.config.secret_key_base = 'f37c0aff04b6048324d0a94cfbafb55bbfed21df873ec27a6801f240416810803a869484099412b8bfd52fed74c78ee70331c66ca289bd8fdfcf520eb6195013'
